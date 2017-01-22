3c39b1f0115c9346ab905b537d4211c7991af378
Matt Ruehle <matthew.t.ruehle@gmail.com>
2017-01-22 12:05:13 -0500

    Refactoring into functions.

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -6,38 +6,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void get_input(char * destination) {
+	puts("Enter the card_name: ");
+	scanf("%2s", destination);
+	return;
+}
+
+void process_count(int * count, int val) {
+	if ((val > 2) && (val < 7)) {
+		*count = *count + 1;
+	} else if (val == 10) {
+		*count = *count - 1;
+	}
+	printf("Current count: %i\n", *count);
+	return;
+}
+
+void card_to_value(char *card_name, int * val) {
+	switch (toupper(card_name[0])) {
+		case 'K':
+		case 'Q':
+		case 'J':
+			*val = 10;
+			break;
+		case 'A':
+			*val = 11;
+			break;
+		case 'X':
+			puts("Closing...");
+			*val = -1;
+			break;
+		default:
+			*val = atoi(card_name);
+			if ((*val < 1) || (*val > 10)) {
+				puts("I don't understand that value!");
+			}
+			break;
+		}
+}
+
 int main()
 {
 	char card_name[3];
 	int count = 0;
-	while (card_name[0] != 'X') {
-		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
-		int val = 0;
-		switch (card_name[0]) {
-			case 'K':
-			case 'Q':
-			case 'J':
-				val = 10;
-				break;
-			case 'A':
-				val = 11;
-				break;
-			case 'X':
-				continue;
-			default:
-				val = atoi(card_name);
-				if ((val < 1) || (val > 10)) {
-					puts("I don't understand that value!");
-					continue;
-				}
-		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--;
-		}
-		printf("Current count: %i\n", count);
+	int val;
+	while (card_name[0] != 'X' && card_name[0] != 'x') {
+		get_input(card_name);
+		card_to_value(card_name, &val);
+		process_count(&count, val);
 	}
 	return 0;
 }
+
