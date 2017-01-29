9e75a7402f1d6f29aa054ca3d51c846ba786947f
Matt Ruehle <matthew.t.ruehle@gmail.com>
2017-01-29 14:29:45 -0500

    web_server syntax, capitalization

--- exercises/web_server/tcpserv01.c
+++ exercises/web_server/tcpserv01.c
@@ -8,30 +8,30 @@ int main(int argc, char **argv)
     socklen_t clilen;
     struct sockaddr_in cliaddr, servaddr;
 
-    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
     
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family      = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port        = htons(SERV_PORT);
 
-    Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-    Listen(listenfd, LISTENQ);
+    listen(listenfd, LISTENQ);
     printf("Listening on port %d\n", servaddr.sin_port);
 
     for ( ; ; ) {
 	clilen = sizeof(cliaddr);
-	connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+	connfd = accept(listenfd, (SA *) &cliaddr, &clilen);
 	printf("Accepted\n");
 	
-	if ( (childpid = Fork()) == 0) {
+	if ( (childpid = fork()) == 0) {
 	    printf("Forked\n");
-	    Close(listenfd);
-	    str_echo(connfd);
+	    close(listenfd);
+	    printf("%d", connfd);
 	    exit(0);
 	}
-	Close(connfd);
+	close(connfd);
     }
 }
 
